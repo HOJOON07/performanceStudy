3f47ee1b02d5e094efd708868448d51787850ab5
HOJOON07 <ghwns107@naver.com>
2023-07-16 14:26:29 +0900

    1,2

---
+++ Desktop/performance/lecture-1/README.md
@@ -0,0 +1,44 @@
+
+
+### 실행
+
+1. download sources
+
+```
+$ git clone https://github.com/performance-lecture/lecture-1.git
+```
+
+2. install dependencies
+
+```
+$ npm install
+or
+$ yarn
+```
+
+3. start development server
+
+```
+$ npm run start
+or
+$ yarn start
+```
+
+4. start json-server
+
+```
+$ npm run server
+or
+$ yarn server
+```
+*3번의 dev server 와 다른 콘솔에서 띄워줍니다.
+
+5. build + serve
+
+```
+$ npm run serve
+or
+$ yarn serve
+```
+
+
---
+++ Desktop/performance/lecture-1/src/App.js
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import ListPage from "./pages/ListPage/index";
+import ViewPage from "./pages/ViewPage/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Suspense fallback={<div>...로딩중</div>}>
+        <Switch>
+          <Route path="/" component={ListPage} exact />
+          <Route path="/view/:id" component={ViewPage} exact />
+        </Switch>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
